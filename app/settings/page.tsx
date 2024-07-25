541618c7c68452f19d1c24de5910a730f963bc02
Parker Hutcheson <72584282+parkerhutcheson@users.noreply.github.com>
2024-07-25 23:29:12 +0900

    Settings setup

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -5,6 +5,7 @@ import UserTable from "@/app/components/UserTable"
 import { getAllUsers } from "@/app/lib/db"
 import Link from 'next/link'
 import { PlusCircleIcon } from '@heroicons/react/24/solid'
+import SettingsTable from "@/app/components/SettingsTable"
 
 export default async function AdminPortal() {
   const session = await auth()
@@ -13,20 +14,12 @@ export default async function AdminPortal() {
     redirect('/api/auth/signin')
   }
 
-  if (!session?.user?.admin) {
-    return <AccessDenied />
-  }
-
   const users = await getAllUsers()
 
   return (
     <div className="bg-primary min-h-screen p-8">
-      <h1 className="text-2xl font-semibold text-white ml-12">Administration Portal</h1>
-      <Link href="/administration/new-user" className="inline-flex items-center ml-12 mt-4 text-accent hover:text-accent2 transition-colors duration-200">
-        <PlusCircleIcon className="h-5 w-5 mr-2" />
-        New User
-      </Link>
-      <UserTable users={users} />
+      <h1 className="text-2xl font-semibold text-white ml-12">Settings</h1>
+      <SettingsTable users={users} />
     </div>
   )
 }
